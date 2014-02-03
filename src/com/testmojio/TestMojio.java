3a0875064ff637f20ae3f4ce895657ac0970ece1
vchiu86 <vincentchiu86@gmail.com>
2014-02-02 19:19:39 -0800

    Tests to see if notifications, maps, and settings page loads.  Also tests to see if each button on settings page can be pressed.

--- src/com/testmojio/TestMojio.java
+++ src/com/testmojio/TestMojio.java
@@ -30,33 +30,77 @@ public class TestMojio extends ActivityInstrumentationTestCase2 {
 
 	private Solo solo;
 	
+	//Enter credentials and click log in before each test
 	protected void setUp() throws Exception {
-		//super.setUp();
 		solo = new Solo(getInstrumentation(), getActivity());
 		solo.enterText(0, "vchiu86");
 		solo.enterText(1, "capstone");
 		solo.clickOnButton("Log In");
 	}
 	
+	//Test to see whether Notifications page loads or not
 	public void testNotificationsActivity() {
+		//Navigate to and make sure we are on the Notifications page 
+		solo.waitForActivity("MainMenuActivity");
 		solo.clickOnButton("Notifications");
+		solo.waitForActivity("NotificationsActivity");
 		assertTrue(solo.searchText("Date"));
 	}
 	
-
+	//Test to see whether the Map page loads or not
 	public void testMapActivity() {
+		//Navigate to and make sure we are on the Maps page 
+		solo.waitForActivity("MainMenuActivity");
 		solo.clickOnButton("Map");
+		solo.waitForActivity("MapsActivity");
 		assertTrue(solo.searchText("Location"));
 	}
 	
+	//Test to see whether the Settings page loads or not and to make sure the buttons/checkboxes can be pressed
 	public void testSettingsActivity() {
+		boolean boxStatus;
+		
+		//Navigate to and make sure we are on the Subscriptions page 
+		solo.waitForActivity("MainMenuActivity");
 		solo.clickOnButton("Settings");
+		solo.waitForActivity("SettingsActivity");
 		assertTrue(solo.searchText("Subscriptions"));
+	
+		//Test to see if the status of the notifications button is different after pressing
+		boxStatus = solo.isToggleButtonChecked(0);
+		solo.clickOnButton(0);
+		assertTrue(solo.isToggleButtonChecked(0) != boxStatus);
+		boxStatus = solo.isToggleButtonChecked(0);
+		solo.clickOnButton(0);
+		assertTrue(solo.isToggleButtonChecked(0) != boxStatus);
+		
+		//Test to see if the sound setting checkbox is different after pressing
+		boxStatus = solo.isCheckBoxChecked(0);
+		solo.clickOnButton(1);
+		assertTrue(solo.isCheckBoxChecked(0) != boxStatus);
+		boxStatus = solo.isCheckBoxChecked(0);
+		solo.clickOnButton(1);
+		assertTrue(solo.isCheckBoxChecked(0) != boxStatus);
+		
+		//Test to see if the vibration setting checkbox is different after pressing
+		boxStatus = solo.isCheckBoxChecked(1);
+		solo.clickOnButton(2);
+		assertTrue(solo.isCheckBoxChecked(1) != boxStatus);
+		boxStatus = solo.isCheckBoxChecked(1);
+		solo.clickOnButton(2);
+		assertTrue(solo.isCheckBoxChecked(1) != boxStatus);
+		
+		//Check to see if the status of the subscription status of the first dongle is different after pressing
+		boxStatus = solo.isToggleButtonChecked(1);
+		solo.clickOnButton(3);
+		assertTrue(solo.isToggleButtonChecked(1) != boxStatus);
+		boxStatus = solo.isToggleButtonChecked(1);
+		solo.clickOnButton(3);
+		assertTrue(solo.isToggleButtonChecked(1) != boxStatus);
 	}
 		
-
+	//After each test, go back to the main menu and click log out
 	protected void tearDown() throws Exception {
-		//super.tearDown();
 		solo.goBack();
 		solo.clickOnButton("Log Out");
 		solo.finishOpenedActivities();
