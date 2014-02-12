9214ca383f12baa477a43972fa7f6cee28e6ba3a
vchiu86 <vincentchiu86@gmail.com>
2014-02-11 20:15:49 -0800

    Time between login tests increased

--- src/com/testmojio/LoginTest.java
+++ src/com/testmojio/LoginTest.java
@@ -70,7 +70,7 @@ public class LoginTest extends ActivityInstrumentationTestCase2 {
 	
 	//Wait 3 seconds between each test case
 	protected void tearDown() throws Exception {
-		solo.sleep(5000);
+		solo.sleep(7000);
 	}
 
 }
